5271aa9791b7867f48c347722e9623772f85c789
Mamy Raobelina <mamyraoby@outlook.com>
2024-06-16 20:13:59 +0300

    Remove unchaged attributes from ProxyMiddleware

--- src/Middleware/ProxyMiddleware.php
+++ src/Middleware/ProxyMiddleware.php
@@ -2,21 +2,10 @@
 
 namespace MamyRaoby\LaravelProxy\Middleware;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Http\Middleware\TrustProxies;
 
 class ProxyMiddleware extends TrustProxies {
-    /**
-     * The trusted proxies headers for the application.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_FOR |
-                         Request::HEADER_X_FORWARDED_HOST |
-                         Request::HEADER_X_FORWARDED_PORT |
-                         Request::HEADER_X_FORWARDED_PROTO |
-                         Request::HEADER_X_FORWARDED_AWS_ELB;
                          
     public function __construct()
     {
